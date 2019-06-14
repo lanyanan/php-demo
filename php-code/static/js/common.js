a4e2f0961083bebd89a87d5101c07b7672882a0f
heliu <heliu@gemdale.com>
2019-06-14 10:03:46 +0800

    home页面的点击事件

--- php-code/static/js/common.js
+++ php-code/static/js/common.js
@@ -1,7 +1,7 @@
 $(function(){
-}).on('click', '.result-page-logo,.result-bottom .home', function(){
+}).on('click', '.home-bottom,.result-page-logo,.result-bottom .home', function(){
 	window.location.href = '/mobile/home';
-}).on('click', '.result-bottom .loveSearch', function(){
+}).on('click', '.home-bottom,.result-bottom .loveSearch', function(){
 	window.location.href = '/mobile/loveSearch';
 });
 

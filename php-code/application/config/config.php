1f04396b8be47df593da6b077ab757a32e515e25
天使漫步 <landsnail@qq.com>
2019-07-10 10:41:22 +0800

    no message

--- php-code/application/config/config.php
+++ php-code/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://dev.zizaiplus.com/';
+$config['base_url'] = 'http://dawn.inewhome.com/';
 
 /*
 |--------------------------------------------------------------------------
